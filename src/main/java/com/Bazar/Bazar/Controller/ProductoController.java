8180fb5182e7c804da7772f6d5d9f963ebac28d7
Marcemanator <96085088+Marcemanator@users.noreply.github.com>
2024-10-08 23:07:56 +0200

    Arreglada la conexion con la bbdd y probado el modelo crear de cliente quedan todos los modelos por probar

--- src/main/java/com/Bazar/Bazar/Controller/ProductoController.java
+++ src/main/java/com/Bazar/Bazar/Controller/ProductoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class ProductoController {
     
     
     @PostMapping("productos/crear")
-    public void crearProducto(Producto pro){
+    public void crearProducto(@RequestBody Producto pro){
         
         Proserv.crearProducto(pro);
     }
--- src/main/java/com/Bazar/Bazar/Controller/VentaController.java
+++ src/main/java/com/Bazar/Bazar/Controller/VentaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class VentaController {
     VentaService Venserv;
     
     @PostMapping("/ventas/crear")
-    public void crearVentas(Venta ven){
+    public void crearVentas(@RequestBody Venta ven){
         
         Venserv.crearVenta(ven);
     }
